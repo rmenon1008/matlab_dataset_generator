afd0b9cd89c785a41d2d6eebd3ee198cef84e30b
patorrad <paolo.a.torrado@gmail.com>
2023-10-26 16:38:55 -0700

    Fixed sequence gen - all within bounds

--- machine_learning/rnn_pt.py
+++ machine_learning/rnn_pt.py
@@ -18,7 +18,7 @@ np.set_printoptions(threshold=sys.maxsize)
 
 # Specify the path to your HDF5 file
 if platform.system() == 'Windows':
-    hdf5_file_path = 'matlab_dataset_generator/dataset.h5'
+    hdf5_file_path = '..\dataset_0_5m_spacing.h5'
 else:
     hdf5_file_path = '../dataset_0_5m_spacing.h5'
 
@@ -42,8 +42,6 @@ with h5py.File(hdf5_file_path, 'r') as file:
     # Read the data into a NumPy array
     csis_mag = csis_mag[:]
     positions = positions[:]
-    cprint.info(f'csis_mag {csis_mag.shape}')
-    cprint.info(f'positions {positions.shape}')
 
 if SCALE_DATASET:
     # cprint.info(csis_mag)
@@ -69,6 +67,7 @@ if SCALE_DATASET:
 
 # Define the sequence length
 sequence_length = 10
+grid_length = int(np.sqrt(csis_mag_scaled.size(1)))
 
 # Calculate the number of sequences that can be obtained
 num_sequences = csis_mag_scaled.size(1) // sequence_length
@@ -78,10 +77,11 @@ split_sequences = []
 
 # Loop through the input tensor and split it into sequences
 for i in range(num_sequences):
-    start = i * sequence_length
-    end = (i + 1) * sequence_length
-    sequence = csis_mag_scaled[:, start:end]
-    split_sequences.append(sequence)
+    if i % grid_length < grid_length - sequence_length:
+        start = i * sequence_length
+        end = (i + 1) * sequence_length
+        sequence = csis_mag_scaled[:, start:end]
+        split_sequences.append(sequence)
 
 # Convert 'split_sequences' to a PyTorch tensor if needed
 split_sequences_tensor = torch.stack(split_sequences, dim=2).T
@@ -126,155 +126,155 @@ for i, batch in enumerate(dataloader, 0):
     # cprint.info(batch[1].shape)
     cprint(f'Batch {i} size {batch[0].shape[0]}')  # Check the batch size
 
-# Define a simple RNN class
-class RNN(nn.Module):
-    def __init__(self, input_size, hidden_size, num_layers, output_size, dropout_prob):
-        super(RNN, self).__init__()
-        self.hidden_size = hidden_size
-        self.num_layers = num_layers
-        self.rnn = nn.RNN(input_size, hidden_size, num_layers, batch_first=True, dropout=dropout_prob)
-        self.fc = nn.Linear(hidden_size, output_size)
+# # Define a simple RNN class
+# class RNN(nn.Module):
+#     def __init__(self, input_size, hidden_size, num_layers, output_size, dropout_prob):
+#         super(RNN, self).__init__()
+#         self.hidden_size = hidden_size
+#         self.num_layers = num_layers
+#         self.rnn = nn.RNN(input_size, hidden_size, num_layers, batch_first=True, dropout=dropout_prob)
+#         self.fc = nn.Linear(hidden_size, output_size)
 
-    def forward(self, x):
-        # Initialize hidden state with zeros
-        h0 = torch.zeros(self.num_layers, x.size(0), self.hidden_size).to(x.device)
+#     def forward(self, x):
+#         # Initialize hidden state with zeros
+#         h0 = torch.zeros(self.num_layers, x.size(0), self.hidden_size).to(x.device)
         
-        # Forward pass through the RNN layer
-        out, _ = self.rnn(x, h0)
+#         # Forward pass through the RNN layer
+#         out, _ = self.rnn(x, h0)
         
-        # Reshape the output for the fully connected layer
-        out = out[:, -1, :]
-        out = self.fc(out)
-        return out
+#         # Reshape the output for the fully connected layer
+#         out = out[:, -1, :]
+#         out = self.fc(out)
+#         return out
     
-# Define a simple LSTM class
-class LSTM(nn.Module):
-    def __init__(self, input_size, hidden_size, num_layers, output_size, dropout_prob):
-        super(LSTM, self).__init__()
-        self.hidden_size = hidden_size
-        self.num_layers = num_layers
-        self.lstm = nn.LSTM(input_size, hidden_size, num_layers, batch_first=True, dropout=dropout_prob)
-        self.fc = nn.Linear(hidden_size, output_size)
+# # Define a simple LSTM class
+# class LSTM(nn.Module):
+#     def __init__(self, input_size, hidden_size, num_layers, output_size, dropout_prob):
+#         super(LSTM, self).__init__()
+#         self.hidden_size = hidden_size
+#         self.num_layers = num_layers
+#         self.lstm = nn.LSTM(input_size, hidden_size, num_layers, batch_first=True, dropout=dropout_prob)
+#         self.fc = nn.Linear(hidden_size, output_size)
 
-    def forward(self, x):
-        # Initialize hidden state with zeros
-        h0 = torch.zeros(self.num_layers, x.size(0), self.hidden_size).to(x.device)
-        c0 = torch.zeros(self.num_layers, x.size(0), self.hidden_size).to(x.device)
+#     def forward(self, x):
+#         # Initialize hidden state with zeros
+#         h0 = torch.zeros(self.num_layers, x.size(0), self.hidden_size).to(x.device)
+#         c0 = torch.zeros(self.num_layers, x.size(0), self.hidden_size).to(x.device)
         
-        # Forward pass through the LSTM layer
-        out, _ = self.lstm(x, (h0, c0))
+#         # Forward pass through the LSTM layer
+#         out, _ = self.lstm(x, (h0, c0))
         
-        # Reshape the output for the fully connected layer
-        out = self.fc(out[:, -1, :])
-        return out
+#         # Reshape the output for the fully connected layer
+#         out = self.fc(out[:, -1, :])
+#         return out
     
-# Define a simple GRU class
-class GRU(nn.Module):
-    def __init__(self, input_size, hidden_size, num_layers, output_size, dropout_prob):
-        super(GRU, self).__init__()
-        self.hidden_size = hidden_size
-        self.num_layers = num_layers
-        self.gru = nn.GRU(input_size, hidden_size, num_layers, batch_first=True, dropout=dropout_prob)
-        self.fc = nn.Linear(hidden_size, output_size)
+# # Define a simple GRU class
+# class GRU(nn.Module):
+#     def __init__(self, input_size, hidden_size, num_layers, output_size, dropout_prob):
+#         super(GRU, self).__init__()
+#         self.hidden_size = hidden_size
+#         self.num_layers = num_layers
+#         self.gru = nn.GRU(input_size, hidden_size, num_layers, batch_first=True, dropout=dropout_prob)
+#         self.fc = nn.Linear(hidden_size, output_size)
 
-    def forward(self, x):
-        # Initialize hidden state with zeros
-        h0 = torch.zeros(self.num_layers, x.size(0), self.hidden_size).to(x.device)
+#     def forward(self, x):
+#         # Initialize hidden state with zeros
+#         h0 = torch.zeros(self.num_layers, x.size(0), self.hidden_size).to(x.device)
         
-        # Forward pass through the GRU layer
-        out, _ = self.gru(x, h0)
+#         # Forward pass through the GRU layer
+#         out, _ = self.gru(x, h0)
         
-        # Reshape the output for the fully connected layer
-        out = self.fc(out[:, -1, :])
-        return out
+#         # Reshape the output for the fully connected layer
+#         out = self.fc(out[:, -1, :])
+#         return out
 
-def get_model(model, model_params):
-    models = {
-        "rnn": RNN,
-        "lstm": LSTM,
-        "gru": GRU,
-    }
-    return models.get(model.lower())(**model_params)
+# def get_model(model, model_params):
+#     models = {
+#         "rnn": RNN,
+#         "lstm": LSTM,
+#         "gru": GRU,
+#     }
+#     return models.get(model.lower())(**model_params)
 
-# Hyperparameters
-input_size = 128
-hidden_size = 64
-num_layers = 5
-output_size = 128
-sequence_length = 9
-learning_rate = 0.0001
-dropout = .2
-num_epochs = 1000
-batch_size = batch_size
+# # Hyperparameters
+# input_size = 128
+# hidden_size = 64
+# num_layers = 5
+# output_size = 128
+# sequence_length = 9
+# learning_rate = 0.0001
+# dropout = .2
+# num_epochs = 1000
+# batch_size = batch_size
 
-model_params = {'input_size': input_size,
-                'hidden_size' : hidden_size,
-                'num_layers' : num_layers,
-                'output_size' : output_size,
-                'dropout_prob' : dropout}
-current = datetime.datetime.now()
-writer = SummaryWriter(f"runs/test{num_epochs}_{num_layers}_{learning_rate}_{dropout}_{current.month}-{current.day}-{current.hour}-{current.minute}")
-# writer.add_text(
-#     "hyperparameters",
-#     "|param|value|\n|-|-|\n%s" % ("\n".join([f"|{key}|{value}|" for key, value in vars(args).items()])),
+# model_params = {'input_size': input_size,
+#                 'hidden_size' : hidden_size,
+#                 'num_layers' : num_layers,
+#                 'output_size' : output_size,
+#                 'dropout_prob' : dropout}
+# current = datetime.datetime.now()
+# writer = SummaryWriter(f"runs/test{num_epochs}_{num_layers}_{learning_rate}_{dropout}_{current.month}-{current.day}-{current.hour}-{current.minute}")
+# # writer.add_text(
+# #     "hyperparameters",
+# #     "|param|value|\n|-|-|\n%s" % ("\n".join([f"|{key}|{value}|" for key, value in vars(args).items()])),
 
-# Create a simple RNN model
-model = get_model("RNN", model_params)
+# # Create a simple RNN model
+# model = get_model("RNN", model_params)
 
-# Loss and optimizer
-criterion = nn.MSELoss()
-optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
+# # Loss and optimizer
+# criterion = nn.MSELoss()
+# optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
 
-# Training loop
-model = model.float()
-i = 0
-for epoch in range(num_epochs):
-    running_loss = 0.0
-    for batch in dataloader:
-        sequences, targets = batch
-        # cprint.warn(sequences.shape)
-        # cprint.warn(targets.shape)
-        outputs = model(sequences.float())
-        # cprint.warn(f'outputs {outputs.shape}')
-        # cprint.warn(f'targets {targets.shape}')
-        loss = criterion(outputs, targets.float())
+# # Training loop
+# model = model.float()
+# i = 0
+# for epoch in range(num_epochs):
+#     running_loss = 0.0
+#     for batch in dataloader:
+#         sequences, targets = batch
+#         # cprint.warn(sequences.shape)
+#         # cprint.warn(targets.shape)
+#         outputs = model(sequences.float())
+#         # cprint.warn(f'outputs {outputs.shape}')
+#         # cprint.warn(f'targets {targets.shape}')
+#         loss = criterion(outputs, targets.float())
         
-        optimizer.zero_grad()
-        loss.backward()
-        optimizer.step()
-        # print statistics
-        running_loss += loss.item()
-        writer.add_scalar("losses/running_loss", loss.item(), i)
-        # if (epoch) % 100 == 99:
-        #     print(f'Epoch [{epoch}/{num_epochs}], Loss: {loss.item():.4f}')
-        if (i) % 100 == 99:    # print every 2000 mini-batches
-            print(f'[{epoch + 1}, {num_epochs}] loss: {running_loss:.3f}')
-            running_loss = 0.0
-        i += 1
+#         optimizer.zero_grad()
+#         loss.backward()
+#         optimizer.step()
+#         # print statistics
+#         running_loss += loss.item()
+#         writer.add_scalar("losses/running_loss", loss.item(), i)
+#         # if (epoch) % 100 == 99:
+#         #     print(f'Epoch [{epoch}/{num_epochs}], Loss: {loss.item():.4f}')
+#         if (i) % 100 == 99:    # print every 2000 mini-batches
+#             print(f'[{epoch + 1}, {num_epochs}] loss: {running_loss:.3f}')
+#             running_loss = 0.0
+#         i += 1
 
-for i in range(10):
-    # To use the trained model for prediction, you can pass new sequences to the model:
-    # new_input = torch.randn(1, sequence_length, input_size)
-    new_input = split_sequences_tensor[torch.randint(0, split_sequences_tensor.shape[0], (1,)),:,:]
+# for i in range(10):
+#     # To use the trained model for prediction, you can pass new sequences to the model:
+#     # new_input = torch.randn(1, sequence_length, input_size)
+#     new_input = split_sequences_tensor[torch.randint(0, split_sequences_tensor.shape[0], (1,)),:,:]
 
-    # Prediction
-    prediction = model(new_input.to(torch.float32))
+#     # Prediction
+#     prediction = model(new_input.to(torch.float32))
 
-    # Graphs
-    fig, axs = plt.subplots(4)
-    new_input = new_input.squeeze()
-    cprint.warn(new_input[9,:].shape)
-    axs[0].plot(new_input[7,:])
-    axs[1].plot(new_input[8,:])
-    axs[2].plot(new_input[9,:])
+#     # Graphs
+#     fig, axs = plt.subplots(4)
+#     new_input = new_input.squeeze()
+#     cprint.warn(new_input[9,:].shape)
+#     axs[0].plot(new_input[7,:])
+#     axs[1].plot(new_input[8,:])
+#     axs[2].plot(new_input[9,:])
 
-    prediction = prediction.detach().numpy()
-    cprint.info(prediction)
-    cprint.info(prediction.size)
-    axs[3].plot(prediction.squeeze())
-    axs[3].set_title("Prediction")
-    plt.show()
+#     prediction = prediction.detach().numpy()
+#     cprint.info(prediction)
+#     cprint.info(prediction.size)
+#     axs[3].plot(prediction.squeeze())
+#     axs[3].set_title("Prediction")
+#     plt.show()
 
-writer.close()
+# writer.close()
 
-# NOTE Run "tensorboard --logdir runs" to see results
+# # NOTE Run "tensorboard --logdir runs" to see results
